3fa462d0ebff9687b70284eb26ee12e45bb20388
NateBalducciSound <natejbal@gmail.com>
2025-11-23 18:13:36 -0800

    change to phaseA and arrowmovement

--- Assets/Scripts/QuestionsClass.cs
+++ Assets/Scripts/QuestionsClass.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class QuestionsClass : MonoBehaviour
+public class QuestionsClass : GameManager
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
